20034a5c723b91e35d46dc82ca232b13c924d8a9
DaryaA <12345@nail.com>
2024-12-09 20:30:11 +0500

    «start»

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <header>
+    
+  </header>
+
+  <main>
+    <div class="container">
+      <h5 class="fs-1">vjdfnvndjknv</h5>
+      <a href="#!">ggghghghhghgh</a>
+    </div>
+
+  </main>
+</template>
+
+<style lang="scss"></style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import "/node_modules/bootstrap/dist/css/bootstrap.css"
+import './assets/main.scss'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// import "bootstrap/js"
